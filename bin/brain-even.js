708675ca91ef423955e8dfe25083b8edfa04926b
Valentino-vada <tataurov_qi@yahoo.com>
2021-04-06 22:58:10 +0300

    add brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -5,5 +5,13 @@ import readlineSync from 'readline-sync';
 
 console.log(`Hello! ${name}`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
-console.log(`Question: ${Math.floor(Math.random() * 100)}`);
-const ansver = readlineSync.question('Your answer: ');
+const randomNum = Math.floor(Math.random() * 100);
+console.log(`Question: ${randomNum}`);
+const acceptAnswer = readlineSync.question('Your answer: ');
+console.log(`${acceptAnswer}`);
+const otr = () => {
+	const answer = ['yes', 'no'];
+if (randomNum % 2 === 0 && answer === answer[0]) {
+	  return 'Correct!';
+}
+}
