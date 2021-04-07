a7e0b86cbf7d7201667a0e868ada57d7b927c3a6
Valentino-vada <tataurov_qi@yahoo.com>
2021-04-07 22:48:49 +0300

    changed brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -6,12 +6,25 @@ import readlineSync from 'readline-sync';
 console.log(`Hello! ${name}`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 const randomNum = Math.floor(Math.random() * 100);
-console.log(`Question: ${randomNum}`);
-const acceptAnswer = readlineSync.question('Your answer: ');
-console.log(`${acceptAnswer}`);
-const otr = () => {
-	const answer = ['yes', 'no'];
-if (randomNum % 2 === 0 && answer === answer[0]) {
-	  return 'Correct!';
+console.log(`Question: ${randomNum}`);	
+const displayResultOfQuestions = (answ1, answ2) => {
+
+	for (let i = 0; i < 3; i++) {
+		
+	const acceptAnswer = readlineSync.question('Your answer: ');
+	
+	if (randomNum % 2 === 0 && acceptAnswer === 'yes') {
+		console.log('Correct!');
+		console.log(`Question: ${randomNum}`);
+	} else if (randomNum % 2 === 1 && acceptAnswer === 'no') {
+		console.log('Correct!');
+		console.log(`Question: ${randomNum}`);
+	} else {
+		return `'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}`;
+	}
+	
 }
-}
+return`Congratulations, ${name}`;
+	}
+	
+console.log(displayResultOfQuestions('yes', 'no'));
