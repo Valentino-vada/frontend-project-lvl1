4dca495d7088dcba5fd73f6ad561bae38c3f336f
Valentino-vada <tataurov_qi@yahoo.com>
2021-04-10 13:38:12 +0300

    changed brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,31 +1,31 @@
 #!/usr/bin/env node
 
-import name from '../src/cli.js';
 import readlineSync from 'readline-sync';
+import name from '../src/cli.js';
 
 console.log(`Hello! ${name}`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
-const randomNum = Math.floor(Math.random() * 100);	 
+const randomNum = Math.floor(Math.random() * 10); 
 console.log(`Question: ${randomNum}`);
+
 const displayResultOfQuestions = (answ1, answ2) => {
-	const randomNum1 = Math.floor(Math.random() * 100);
 	
-
-	for (let i = 0; i < 3; i += 1) {	
-	const acceptAnswer = readlineSync.question('Your answer: ');
-	if (randomNum1 % 2 === 0 && acceptAnswer === 'yes') {
-		console.log('Correct!');
-		console.log(`Question: ${randomNum1}`);
-	} else if (randomNum1 % 2 === 1 && acceptAnswer === 'no') {
-		console.log('Correct!');
-		console.log(`Question: ${randomNum1}`);
-	} else {
-		return `'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}`;
-	}	
-	
+for (let i = 0; i < 3; i += 1) { 
+	const m = console.count('Correct!');
+	const randomNum1 = [20, 33];
+const acceptAnswer = readlineSync.question('Your answer: ');
+if (randomNum1[0] % 2 === 0 && acceptAnswer === 'yes') {
+//console.count('Correct!');
+m;
+console.log(`Question: ${randomNum1[0]}`);
+} else if (randomNum1[1] % 2 === 1 && acceptAnswer === 'no') {
+	//console.count('Correct!');
+	m;
+console.log(`Question: ${randomNum1[1]}`);
+} else {
+return `'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}`;
+} 
 }
-return (`Congratulations, ${name}!`);
-
-	}
-	console.log(displayResultOfQuestions('yes', 'no'));
-
+return `Congratulations, ${name}!`;
+}
+console.log(displayResultOfQuestions('yes', 'no'));
