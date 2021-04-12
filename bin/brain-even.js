3012a512e3e4ca60a5c04bb07d029db9ac53d337
Valentino-vada <tataurov_qi@yahoo.com>
2021-04-12 20:53:32 +0300

    changed brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -5,27 +5,29 @@ import name from '../src/cli.js';
 
 console.log(`Hello! ${name}`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
-const randomNum = Math.floor(Math.random() * 10); 
+const randomNum = Math.ceil(Math.random() * 51);
 console.log(`Question: ${randomNum}`);
-
-const displayResultOfQuestions = (answ1, answ2) => {
-	
-for (let i = 0; i < 3; i += 1) { 
-	const m = console.count('Correct!');
-	const randomNum1 = [20, 33];
-const acceptAnswer = readlineSync.question('Your answer: ');
-if (randomNum1[0] % 2 === 0 && acceptAnswer === 'yes') {
-//console.count('Correct!');
-m;
-console.log(`Question: ${randomNum1[0]}`);
-} else if (randomNum1[1] % 2 === 1 && acceptAnswer === 'no') {
-	//console.count('Correct!');
-	m;
-console.log(`Question: ${randomNum1[1]}`);
-} else {
-return `'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}`;
-} 
-}
-return `Congratulations, ${name}!`;
-}
-console.log(displayResultOfQuestions('yes', 'no'));
+const displayResultOfQuestions = () => {
+  let i = 0;
+  for (; i < 3; i += 1) {
+	  // const randomNum = Math.ceil(Math.random() * 51);
+    const acceptAnswer = readlineSync.question('Your answer: ');
+    if (randomNum % 2 === 0 && acceptAnswer === 'yes') {
+      console.log('Correct!');
+      if (i === 2) {
+        continue;
+      }
+      console.log(`Question: ${randomNum}`);
+    } else if (randomNum % 2 === 1 && acceptAnswer === 'no') {
+      console.log('Correct!');
+      if (i === 2) {
+        continue;
+      }
+      console.log(`Question: ${randomNum}`);
+    } else {
+      return `'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}`;
+    }
+  }
+  return `Congratulations, ${name}!`;
+};
+console.log(displayResultOfQuestions());
