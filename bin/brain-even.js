9376f0a28e013869197cd38815ffa735686d31a2
Valentino-vada <tataurov_qi@yahoo.com>
2021-04-15 22:19:28 +0300

    altered brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -5,29 +5,19 @@ import name from '../src/cli.js';
 
 console.log(`Hello! ${name}`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
-const randomNum = Math.ceil(Math.random() * 51);
-console.log(`Question: ${randomNum}`);
 const displayResultOfQuestions = () => {
-  let i = 0;
-  for (; i < 3; i += 1) {
-	  // const randomNum = Math.ceil(Math.random() * 51);
+  for (let i = 0; i < 3; i += 1) {
+    const randomNum = Math.ceil(Math.random() * 51);
+    console.log(`Question: ${randomNum}`);
     const acceptAnswer = readlineSync.question('Your answer: ');
     if (randomNum % 2 === 0 && acceptAnswer === 'yes') {
       console.log('Correct!');
-      if (i === 2) {
-        continue;
-      }
-      console.log(`Question: ${randomNum}`);
     } else if (randomNum % 2 === 1 && acceptAnswer === 'no') {
       console.log('Correct!');
-      if (i === 2) {
-        continue;
-      }
-      console.log(`Question: ${randomNum}`);
     } else {
-      return `'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}`;
+      return console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}`);
     }
   }
-  return `Congratulations, ${name}!`;
+  return console.log(`Congratulations, ${name}!`);
 };
-console.log(displayResultOfQuestions());
+displayResultOfQuestions();
