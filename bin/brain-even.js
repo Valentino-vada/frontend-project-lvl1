1059a8011c21b482463ff38a0ef71e955ba45f9f
Valentino-vada <tataurov_qi@yahoo.com>
2021-04-19 21:15:48 +0300

    added "if"

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -7,7 +7,7 @@ console.log(`Hello! ${name}`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 const displayResultOfQuestions = () => {
   for (let i = 0; i < 3; i += 1) {
-    const randomNum = Math.ceil(Math.random() * 51);
+    const randomNum = Math.floor(Math.random() * 51);
     console.log(`Question: ${randomNum}`);
     const acceptAnswer = readlineSync.question('Your answer: ');
     if (randomNum % 2 === 0 && acceptAnswer === 'yes') {
@@ -15,7 +15,12 @@ const displayResultOfQuestions = () => {
     } else if (randomNum % 2 === 1 && acceptAnswer === 'no') {
       console.log('Correct!');
     } else {
-      return console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}`);
+      if (randomNum % 2 === 0) {
+        return console.log(`'${acceptAnswer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}`);
+      }
+      if (randomNum % 2 === 1) {
+        return console.log(`'${acceptAnswer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}`);
+      }
     }
   }
   return console.log(`Congratulations, ${name}!`);
