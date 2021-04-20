35ea4105c77618e00e541593a6b868e3af1abc02
Valentino-vada <tataurov_qi@yahoo.com>
2021-04-21 00:11:07 +0300

    added brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -11,6 +11,7 @@ const displayResultOfQuestions = () => {
   const randomNumSum = randomNum1 + randomNum2;
   const randomNumDif = randomNum1 - randomNum2;
   const randomNumMult = randomNum1 * randomNum2;
+  if (Math.floor(Math.random()) < 0.33) {
   console.log(`Question: ${randomNum1} + ${randomNum2}`);
   const acceptAnswer1 = readlineSync.question('Your answer: ');
   if (randomNumSum === Number(acceptAnswer1)) {
@@ -18,6 +19,8 @@ const displayResultOfQuestions = () => {
   } else {
     return console.log(`'${acceptAnswer1}' is wrong answer ;(. Correct answer was '${randomNumSum}'.\nLet's try again, ${name}`);
   }
+}
+if (Math.floor(Math.random()) > 0.33 && Math.floor(Math.random()) < 0.66) {
   console.log(`Question: ${randomNum1} - ${randomNum2}`);
   const acceptAnswer2 = readlineSync.question('Your answer: ');
   if (randomNumDif === Number(acceptAnswer2)) {
@@ -25,6 +28,8 @@ const displayResultOfQuestions = () => {
   } else {
     return console.log(`'${acceptAnswer2}' is wrong answer ;(. Correct answer was '${randomNumDif}'.\nLet's try again, ${name}`);
   }
+}
+if (Math.floor(Math.random()) > 0.66 && Math.floor(Math.random()) < 0.99) {
   console.log(`Question: ${randomNum1} * ${randomNum2}`);
   const acceptAnswer3 = readlineSync.question('Your answer: ');
   if (randomNumMult === Number(acceptAnswer3)) {
@@ -32,6 +37,7 @@ const displayResultOfQuestions = () => {
   } else {
     return console.log(`'${acceptAnswer3}' is wrong answer ;(. Correct answer was '${randomNumMult}'.\nLet's try again, ${name}`);
   }
+}
   return console.log(`Congratulations, ${name}!`);
 };
 displayResultOfQuestions();
