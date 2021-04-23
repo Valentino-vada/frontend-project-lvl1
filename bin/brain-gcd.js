715ec7ba013a0fa963a8e459bf1fc5dc9a4c9f48
Valentino-vada <tataurov_qi@yahoo.com>
2021-04-23 22:07:39 +0300

    added function and fix

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -5,3 +5,25 @@ import name from '../src/cli.js';
 
 console.log(`Hello! ${name}`);
 console.log('Find the greatest common divisor of given numbers.');
+const displayResultOfQuestions = () => {
+  for (let i = 0; i < 3; i += 1) {
+    const randomNum1 = Math.floor(Math.random() * 100);
+    const randomNum2 = Math.floor(Math.random() * 100);
+    const gcdReceive = (a, b) => {
+      if (!b) {
+        return a;
+      }
+      return gcdReceive(b, a % b);
+    };
+    const gcdResult = gcdReceive(randomNum1, randomNum2);
+    console.log(`Question: ${randomNum1} ${randomNum2}`);
+    const acceptAnswer = readlineSync.question('Your answer: ');
+    if (gcdResult === Number(acceptAnswer)) {
+      console.log('Correct!');
+    } else {
+      return console.log(`'${acceptAnswer}' is wrong answer ;(. Correct answer was '${gcdResult}'.\nLet's try again, ${name}`);
+    }
+  }
+  return console.log(`Congratulations, ${name}!`);
+};
+displayResultOfQuestions();
