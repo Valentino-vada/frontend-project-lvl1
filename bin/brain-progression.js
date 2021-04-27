0101cf05eab8abad4b926cc4d222cf50ad59e2d8
Valentino-vada <tataurov_qi@yahoo.com>
2021-04-27 22:31:48 +0300

    added work game

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -6,29 +6,29 @@ import name from '../src/cli.js';
 console.log(`Hello! ${name}`);
 console.log('What number is missing in the progression?');
 const displayResultOfQuestions = () => {
-    const displayArrayOfProgression = () => {
-      const arr = [];
-      let numberArray = 5;  //  first
-      const stepArray = 2;
-      let count = 0;
-      while(count !== 10) {
-          arr.push(numberArray);
-          numberArray += stepArray;
-          count += 1;
-      }
-      const randomLocationSign = Math.floor(Math.random() * arr.length - 1);
-      arr.splice(randomLocationSign, 1, '..');
-      console.log(randomLocationSign)
-      return arr.join(' ');
+  for (let i = 0; i < 3; i += 1) {
+    const arr = [];
+    let numberArray = Math.floor(Math.random() * 9); //  first
+    const firstRandomArray = numberArray;
+    const stepArray = Math.ceil(Math.random() * 4);
+    let count = 0;
+    while (count < 10) {
+      arr.push(numberArray);
+      numberArray += stepArray;
+      count += 1;
     }
-    const arrayProgression = displayArrayOfProgression();
-    console.log(`Question: ${arrayProgression}`)
+    const randomLocationSign = Math.floor(Math.random() * arr.length);
+    arr.splice(randomLocationSign, 1, '..');
+    const arrayProgression = arr.join(' ');
+    const correctValue = firstRandomArray + randomLocationSign * stepArray;
+    console.log(`Question: ${arrayProgression}`);
     const acceptAnswer = readlineSync.question('Your answer: ');
-    if (Number(acceptAnswer) === 5 + randomLocationSign * 2) {
+    if (Number(acceptAnswer) === correctValue) {
       console.log('Correct!');
     } else {
-      return console.log(`'${acceptAnswer}' is wrong answer ;(. Correct answer was '${gcdResult}'.\nLet's try again, ${name}`);
+      return console.log(`'${acceptAnswer}' is wrong answer ;(. Correct answer was '${correctValue}'.\nLet's try again, ${name}`);
     }
+  }
   return console.log(`Congratulations, ${name}!`);
 };
 displayResultOfQuestions();
