14aa394c5421af671a6fbc203411b456770a4ab6
Valentino-vada <tataurov_qi@yahoo.com>
2021-04-29 21:01:23 +0300

    fix bin/brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -8,12 +8,12 @@ console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 const displayResultOfQuestions = () => {
   for (let i = 0; i < 3; i += 1) {
     const randomNum = Math.floor(Math.random() * 101);
-    const getIsPrime = (n) => {
-      if (n === 1) {
+    const getIsPrime = (num) => {
+      if (num === 1) {
         return 'no';
       }
-      for (let j = 2; j * j <= n; j += 1) {
-        if (n % j === 0) {
+      for (let j = 2; j * j <= num; j += 1) {
+        if (num % j === 0) {
           return 'no';
         }
       }
@@ -21,17 +21,17 @@ const displayResultOfQuestions = () => {
     };
     const resultAnswer = getIsPrime(randomNum);
     console.log(`Question: ${randomNum}`);
-    const acceptAnswer = readlineSync.question('Your answer: ');
-    if (resultAnswer === 'yes' && acceptAnswer === 'yes') {
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (resultAnswer === 'yes' && userAnswer === 'yes') {
       console.log('Correct!');
-    } else if (resultAnswer === 'no' && acceptAnswer === 'no') {
+    } else if (resultAnswer === 'no' && userAnswer === 'no') {
       console.log('Correct!');
     } else {
       if (resultAnswer === 'yes') {
-        return console.log(`'${acceptAnswer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}`);
+        return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}`);
       }
       if (resultAnswer === 'no') {
-        return console.log(`'${acceptAnswer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}`);
+        return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}`);
       }
     }
   }
