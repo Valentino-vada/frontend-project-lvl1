a48844f913598c2517f6ea95584177f24ba04ee8
Valentino-vada <tataurov_qi@yahoo.com>
2021-05-02 15:53:57 +0300

    changed src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,5 +2,5 @@ import readlineSync from 'readline-sync';
 
 export default () => {
   const greetingAndAnswer = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
-  console.log(`Hello! ${greetingAndAnswer}`);
+  console.log(`Hello, ${greetingAndAnswer}!`);
 };
